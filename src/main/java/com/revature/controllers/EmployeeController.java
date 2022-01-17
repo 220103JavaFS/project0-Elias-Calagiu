5fd1ac1692de75405f09c5985bbb16aecd588396
Elias Calagiu <eliasc81@gmail.com>
2022-01-17 12:31:34 -0800

    created repos package and Employee DAO interface

--- src/main/java/com/revature/controllers/EmployeeController.java
+++ src/main/java/com/revature/controllers/EmployeeController.java
@@ -1,4 +1,27 @@
 package com.revature.controllers;
 
-public class EmployeeController {
+import com.revature.Controller;
+import io.javalin.Javalin;
+
+public class EmployeeController implements Controller {
+
+    private EmployeeService employeeService = new EmployeeService();
+
+//    Handler getAvenger = (ctx) -> {
+//        if(ctx.req.getSession(false)!=null){
+//            String idString = ctx.pathParam("id");
+//            int id = Integer.parseInt(idString);
+//            Avenger avenger = avengerService.callAvenger(id);
+//            ctx.json(avenger);
+//            ctx.status(200);
+//        }else {
+//            ctx.status(401);
+//        }
+//    };
+//
+//    @Override
+//    public void addRoutes(Javalin app) {
+//        app.get("/avenger/{id}", getAvenger);
+//    }
+
 }
---
+++ src/main/java/com/revature/repos/EmployeeDAO.java
@@ -0,0 +1,15 @@
+package com.revature.repos;
+
+import com.revature.models.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+
+    List<Employee> findAll();
+    Employee findEmployee (int id);
+    boolean addEmployee(Employee employee);
+    boolean updateEmployee(Employee employee);
+    boolean deleteEmployee(int id);
+
+}
