12a6a98728827506fcc526eaa94020ea0afac217
Elias Calagiu <eliasc81@gmail.com>
2022-01-17 22:34:51 -0800

    created utils package and ConnectionUtils class to connect to aws rds

--- src/main/java/com/revature/models/Employee.java
+++ src/main/java/com/revature/models/Employee.java
@@ -4,7 +4,8 @@ import java.util.Objects;
 
 public class Employee {
 
-    public Employee(String firstName, String lastName, int salary) {
+    public Employee(Int id, String firstName, String lastName, int salary) {
+        this.id= id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.salary = salary;
---
+++ src/main/java/com/revature/utils/ConnectionUtil.java
@@ -0,0 +1,34 @@
+package com.revature.utils;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionUtil {
+
+    public static Connection getConnection() throws SQLException {
+        //For many framewords using JDBC it is necessary to "register" the driver
+        //package you are using. This is to make the framework aware of it.
+        try {
+            Class.forName(("org.postgresql.Driver"));
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+
+        String url = "employee-management-system.cehcjnxrsoid.us-east-1.rds.amazonaws.com\n";
+        String username = "postgres"; //It is possible and preferable to hide this information in environment variables
+        //System.out.println(System.getenv("SQLPassword"));
+        String password = "password"; //Those are accessed by System.getenv("var-name");
+
+        return DriverManager.getConnection(url, username, password);
+    }
+
+//    public static void main(String[] args){
+//        try{
+//            getConnection();
+//            System.out.println("Connection successful");
+//        }catch (SQLException e){
+//            System.out.println("Connection failed");
+//            e.printStackTrace();
+//        }
+//    }
+}
