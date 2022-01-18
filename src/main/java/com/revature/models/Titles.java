239e462fb089c473a90d1b2970104fcdce7ba8e3
Elias Calagiu <eliasc81@gmail.com>
2022-01-17 22:42:57 -0800

    created Titles model and no arg + parameterized constructors

---
+++ src/main/java/com/revature/models/Titles.java
@@ -0,0 +1,19 @@
+package com.revature.models;
+
+public class Titles {
+
+private int id;
+private String titleName;
+private int titleSalary;
+
+public Titles(){
+
+}
+
+    public Titles(int id, String titleName, int titleSalary) {
+        this.id = id;
+        this.titleName = titleName;
+        this.titleSalary = titleSalary;
+    }
+
+}
