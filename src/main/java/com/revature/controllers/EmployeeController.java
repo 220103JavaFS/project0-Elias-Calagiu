e85d054e44756021a88f4d5c3f22ecf051c0a661
Elias Calagiu <eliasc81@gmail.com>
2022-01-19 22:21:02 -0800

    created methods for implementing employee dao

--- src/main/java/com/revature/controllers/EmployeeController.java
+++ src/main/java/com/revature/controllers/EmployeeController.java
@@ -1,14 +1,20 @@
 package com.revature.controllers;
 
+import com.revature.services.EmployeeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class EmployeeController implements Controller {
+
+    private static Logger log = LoggerFactory.getLogger(EmployeeController.class);
 
     private EmployeeService employeeService = new EmployeeService();
 
-//    Handler getAvenger = (ctx) -> {
+//    Handler getEmployee = (ctx) -> {
 //        if(ctx.req.getSession(false)!=null){
 //            String idString = ctx.pathParam("id");
 //            int id = Integer.parseInt(idString);
-//            Avenger avenger = avengerService.callAvenger(id);
+//            Employee employee = employeeService.callEmployee(id);
 //            ctx.json(avenger);
 //            ctx.status(200);
 //        }else {
--- src/main/java/com/revature/repos/EmployeeDAOImpl.java
+++ src/main/java/com/revature/repos/EmployeeDAOImpl.java
@@ -1,5 +1,33 @@
 package com.revature.repos;
 
+import com.revature.models.Employee;
+
+import java.util.List;
+
 public class EmployeeDAOImpl implements EmployeeDAO{
 
+    @Override
+    public List<Employee> findAll() {
+        return null;
+    }
+
+    @Override
+    public Employee findEmployee(int id) {
+        return null;
+    }
+
+    @Override
+    public boolean addEmployee(Employee employee) {
+        return false;
+    }
+
+    @Override
+    public boolean updateEmployee(Employee employee) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteEmployee(int id) {
+        return false;
+    }
 }
