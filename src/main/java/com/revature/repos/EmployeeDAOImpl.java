53751ad7f265a6592fb093cceb0554a760017f12
Elias Calagiu <eliasc81@gmail.com>
2022-01-19 22:31:41 -0800

    findAll functionality in employeedaoimpl

--- src/main/java/com/revature/repos/EmployeeDAOImpl.java
+++ src/main/java/com/revature/repos/EmployeeDAOImpl.java
@@ -1,14 +1,42 @@
 package com.revature.repos;
 
 import com.revature.models.Employee;
+import com.revature.utils.ConnectionUtil;
+import com.sun.deploy.net.MessageHeader;
 
+import javax.swing.plaf.nimbus.State;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class EmployeeDAOImpl implements EmployeeDAO{
 
     @Override
     public List<Employee> findAll() {
-        return null;
+        try(Connection conn = ConnectionUtil.getConnection()){
+
+            String sql = "SELECT * FROM employees;";
+
+            Statement statement = conn.createStatement();
+
+            ResultSet result = statement.executeQuery(sql);
+
+            List<Employee> list= new ArrayList<>();
+
+            while(result.next()){
+                Employee employee = new Employee();
+                employee.setId(result.getInt("employee_id"));
+                employee.setFirstName(result.getString("first_name"));
+                employee.setLastName(result.getString("last_name"));
+                list.add(employee);
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return new ArrayList<Employee>();
     }
 
     @Override
